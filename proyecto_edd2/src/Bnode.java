35171535ab5951b58dba7e19d6fac9b942752ee9
Your Name <you@example.com>
2020-03-23 20:57:34 -0600

    Btree

---
+++ proyecto_edd2/src/Bnode.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author edas
+ */
+public class Bnode implements Serializable {
+    
+    protected KRRN keys[];//keys 
+    protected int t;//minimun range for keys
+    protected Bnode childs[];//childs of x
+    protected int n;//current number or keys
+    protected boolean leaf;//leaf or not
+
+    public Bnode(int t, boolean leaf) {
+        this.t = t;
+        this.leaf = leaf;
+        keys=new KRRN[2*t-1];
+        childs=new Bnode[2*t];
+        n=0;
+        
+    }
+
+    
+    
+    
+    
+    
+}
---
+++ proyecto_edd2/src/KRRN.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author edas
+ */
+public class KRRN implements Serializable {
+    
+    protected int key;
+    protected int RRN;
+
+    public KRRN() {
+        key=-1;
+        RRN=-1;
+    }
+
+    public KRRN(int key, int RRN) {
+        this.key = key;
+        this.RRN = RRN;
+    }
+
+    
+    
+    
+    
+    
+    
+    
+}
